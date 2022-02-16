8c616aa996a23927de8f3066f8eb199c635c8737
Santiago Corbanini <santiagocorbanini@gmail.com>
2022-02-16 10:39:11 -0300

    Subo proyecto

---
+++ models/Tarea.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const TareaSchema = mongoose.Schema({
+    nombre: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    precio: {
+        type: Number,
+        required: false,
+        default: 0
+    },
+    stock: {
+        type: Number,
+        required: false,
+        default: 0
+    },
+    estado: {
+        type: Boolean,
+        default: false
+    },
+    creado: {
+        type: Date,
+        default: Date.now()
+    }, 
+    proyecto: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Proyecto'
+    }
+});
+
+module.exports = mongoose.model('Tarea', TareaSchema);
